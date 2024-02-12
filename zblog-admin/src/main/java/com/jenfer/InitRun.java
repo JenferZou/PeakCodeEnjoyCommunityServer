56e14c941a47228a771d3b807471b1f423f46207
Jenfer <1634114623@qq.com>
2024-02-12 22:34:33 +0800

    管理端系统设置

--- zblog-admin/src/main/java/com/jenfer/InitRun.java
+++ zblog-admin/src/main/java/com/jenfer/InitRun.java
@@ -6,7 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class InitRun implements ApplicationRunner {
 
     private static final Logger logger = LoggerFactory.getLogger(InitRun.class);
